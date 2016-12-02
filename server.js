aace3d313a6683045a26a61d0f9cabc05eb06fa7
Riccardo Perego <riccardoperego88@gmail.com>
2016-12-02 22:57:06 +0100

    hello world example

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var bodyParser = require("body-parser");
 //var mongodb = require("mongodb");
 //var ObjectID = mongodb.ObjectID;
 
-//var CONTACTS_COLLECTION = "contacts";
+var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+//app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-//var db;
+var db;
 
 // Connect to the database before starting the application server.
 /*mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
@@ -39,29 +39,6 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
-
 app.get("/contacts", function(req, res) {
-  res.status(200);
-});
-
-app.post("/contacts", function(req, res) {
-});
-
-/*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
-
-app.get("/contacts/:id", function(req, res) {
-});
-
-app.put("/contacts/:id", function(req, res) {
-});
-
-app.delete("/contacts/:id", function(req, res) {
+  res.send('Hello World!');
 });
